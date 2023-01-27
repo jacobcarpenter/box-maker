1983ccae3c0e441a5dc6123e999753a055561042
Jacob Carpenter <github@jacobcarpenter.com>
2023-01-26 18:49:17 -0800

    add eslint (and fix lint errors)

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-env node */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+
+		// keep prettier last; https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+		'prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		// TODO: should not be required after the fix for https://github.com/jsx-eslint/eslint-plugin-react/issues/3523 is released
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	plugins: ['react'],
+	rules: {},
+};
